3f30c77102f0a1ad0af27abf6efadd25b327203a
princegobah <babynamek@gmail.com>
2023-03-23 14:49:17 -0400

    added and styled topnav

--- client/src/Search.js
+++ client/src/Search.js
@@ -1,22 +1,23 @@
 import { useHistory } from "react-router-dom"
-import { Fragment, useContext, useEffect, useState } from "react"
+import { Fragment, useContext, useEffect} from "react"
 import { ThemeContext } from "styled-components"
 import Topnav from "./Topnav"
 function Search(){
     const theme=useContext(ThemeContext)
 
 
-useEffect(()=>{if (theme.filteredBlogs.length===0){
+useEffect(()=>
+{if (theme.filteredBlogs.length===0){
     
     fetch('/search')
     .then(res=>res.json())
     .then((res)=>{theme.setFilteredBlogs(res)
- console.log(res)})
- }},[])
+(res)})
+ }},[theme])
 
 const history=useHistory()
 const mapBlogs=theme.filteredBlogs?.map(item=>{
-    console.log()
+  
     return (
         <div key={item.id} className='container'>
             <img src={item.user.image_url}
